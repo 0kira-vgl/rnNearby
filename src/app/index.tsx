8f3c57ea53820da4d052fe183d42b81ab0a674f6
Matheus Tiburcio <matheusgtiburcio@gmail.com>
2024-12-09 21:09:51 -0300

    initial commit

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import "@/styles/global.css";
+import { View, Text } from "react-native";
+export default function App() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
