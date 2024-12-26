f87bbf51873f7f661d20e05a17c4c1357fbb6746
Matheus Tiburcio <matheusgtiburcio@gmail.com>
2024-12-26 12:10:15 -0300

    add cam

--- mobile/src/app/market/[id].tsx
+++ mobile/src/app/market/[id].tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/button";
 import { Loading } from "@/components/loading";
 import { Coupon } from "@/components/market/coupon";
 import { Cover } from "@/components/market/cover";
@@ -5,7 +6,8 @@ import { Details, DetailsProps } from "@/components/market/details";
 import { api } from "@/services/api";
 import { Redirect, router, useLocalSearchParams } from "expo-router";
 import { useEffect, useState } from "react";
-import { Alert, View } from "react-native";
+import { Alert, Modal, View } from "react-native";
+import { useCameraPermissions, CameraView } from "expo-camera";
 
 type DataProps = DetailsProps & {
   cover: string;
@@ -13,9 +15,12 @@ type DataProps = DetailsProps & {
 
 export default function Market() {
   const [data, setData] = useState<DataProps>();
+  const [coupon, setCoupon] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(true);
-
+  const [isVisibleCameraModal, setIsVisibleCameraModal] = useState(false);
+  const [couponIsFetching, setCouponIsFetching] = useState(false);
   const params = useLocalSearchParams<{ id: string }>(); // usado para acessar os parâmetros de consulta
+  const [_, requestPermission] = useCameraPermissions();
 
   async function fetchMarket() {
     try {
@@ -28,6 +33,43 @@ export default function Market() {
         { text: "OK", onPress: () => router.back() },
       ]);
     }
+  }
+
+  async function handleOpenCamera() {
+    try {
+      const { granted } = await requestPermission();
+
+      if (!granted) {
+        return Alert.alert(
+          "Permissão de câmera",
+          "Precisamos que habilite o uso da câmera."
+        );
+      }
+
+      setIsVisibleCameraModal(true); // estado update
+    } catch (error) {
+      console.log(error);
+      Alert.alert("câmera", "Não foi possível utilizar a câmera.");
+    }
+  }
+
+  async function getCoupon(id: string) {
+    try {
+      setCouponIsFetching(true);
+
+      const { data } = await api.get(`/coupons/${id}`);
+
+      Alert.alert("coupon", data.coupon);
+      setCoupon(data.coupon);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Cupom", "Não foi possível utilizar o cupom.");
+    } finally {
+      setCouponIsFetching(false);
+    }
+
+    // setIsVisibleCameraModal(false); // fecha a câmera
+    // setCoupon(id); // atualiza o cupom
   }
 
   useEffect(() => {
@@ -46,7 +88,32 @@ export default function Market() {
     <View className="flex-1">
       <Cover uri={data.cover} />
       <Details data={data} />
-      <Coupon code="FM4345T6" />
+      {coupon && <Coupon code={coupon} />}
+
+      <View className="p-8">
+        <Button onPress={handleOpenCamera}>
+          <Button.Title>Ler QR Code</Button.Title>
+        </Button>
+      </View>
+
+      {/*estado atual*/}
+      <Modal style={{ flex: 1 }} visible={isVisibleCameraModal}>
+        <CameraView
+          facing="back"
+          onBarcodeScanned={({ data }) => console.log(data)}
+          style={{ flex: 1 }}
+        />
+
+        <View className="absolute bottom-10 left-8 right-8">
+          {/*fecha a camera*/}
+          <Button
+            onPress={() => setIsVisibleCameraModal(false)}
+            isLoading={couponIsFetching}
+          >
+            <Button.Title>Voltar</Button.Title>
+          </Button>
+        </View>
+      </Modal>
     </View>
   );
 }
--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://192.168.1.13:3333",
+  baseURL: "http://192.168.1.14:3333", // dynamic url
   timeout: 700,
 });
